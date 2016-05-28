ff39eab79cdb47bde8eb05a9b3560c8cf91816ac
Ronnie Bjørkelund <ronnie.bjorkelund@gmail.com>
2016-05-28 23:52:23 +0200

    add some code

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-"use strict"
+//"use strict"
 
 
 /*
@@ -35,7 +35,7 @@ function Level(plan) {
   this.actors = []; // current pos and state of dynamic elements
 
   for (var y = 0; y < this.height; y++) {
-    var line = plan[y], gridline = [];
+    var line = plan[y], gridLine = [];
     for (var x = 0; x < this.width; x++) {
       var ch = line[x], fieldType = null;
       var Actor = actorChars[ch];
@@ -45,7 +45,7 @@ function Level(plan) {
         fieldType = "wall";
       else if (ch == "!")
         fieldType == "lava";
-      gridline.push(fieldType);
+      gridLine.push(fieldType);
     }
     this.grid.push(gridLine);
   }
@@ -55,3 +55,53 @@ function Level(plan) {
   })[0];
   this.status = this.finishDelay = null;
 }
+
+
+function Vector(x, y) {
+  this.x = x; this.y = y;
+}
+Vector.prototype.plus = function(other) {
+  return new Vector(this.x + other.x, this.y + other.y);
+};
+Vector.prototype.times = function(factor) {
+  return new Vector(this.x * factor, this.y * factor);
+};
+
+
+var actorChars = {
+  "@": Player,
+  "o": Coin,
+  "=": Lava, "|": Lava, "v": Lava
+};
+
+
+function Player(pos) {
+  this.pos = pos.plus(new Vector(0, -0.5));
+  this.size = new Vector(0.8, 1.5);
+  this.speed = new Vector(0, 0);
+}
+Player.prototype.type = "player";
+
+
+function Lava(pos, ch) {
+  this.pos = pos;
+  this.size = new Vector(1, 1);
+  if (ch == "=") {
+    this.speed = new Vector(2, 0);
+  } else if (ch == "|") {
+    this.speed = new Vector(0, 2);
+  } else if (ch == "v") {
+    this.speed = new Vector(0, 3);
+    this.repeatPos = pos;
+  }
+}
+Lava.prototype.type = "lava";
+
+
+
+function Coin(pos) {
+  this.basePos = this.pos = pos.plus(new Vector(0.2, 0.1));
+  this.size = new Vector(0.6, 0.6);
+  this.wobble = Math.random() * Math.PI * 2;
+}
+Coin.prototype.type = "coin";
